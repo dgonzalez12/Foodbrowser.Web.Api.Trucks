8d873a3f0bf294c9c8b87015b13c2ea23e05a2b2
Danny Gonzalez <glezdanny@hotmail.com>
2021-03-10 00:45:02 -0600

    Create base projects

---
+++ Foodbrowser.Web.Api.Trucks.Test/TrucksApiTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Foodbrowser.Web.Api.Trucks.Test
+{
+    public class TrucksApiTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
